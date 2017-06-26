0a18b274611938296a75234d5bec17857570b088
looktoroot <looktoroot@gmail.com>
2017-06-26 15:48:34 +0700

    fix scrolling bugs

--- src/js/script.js
+++ src/js/script.js
@@ -10254,6 +10254,7 @@ return jQuery;
 
 $(document).ready( function(){
 
+  //открытые меню по клику на гамбургер
   $(".hamburger").click(function(){
     $(".page-header__nav-list").toggleClass("open");
    });
@@ -10261,22 +10262,23 @@ $(document).ready( function(){
   var headerTop = $('.page-header__top').offset().top;
   var headerTextPadding = $('.page-header__text').css('padding-top');
 
+  //фиксация меню
   $(window).scroll(function(){
     if( $(window).scrollTop() > headerTop ) {
       $('.container-for-fixed').addClass('fixed');
       $('.page-header__text').css('padding-top', $('.page-header__top').innerHeight());
     }
-
     else {
       $('.container-for-fixed').removeClass('fixed');
       $('.page-header__text').css('padding-top', headerTextPadding);
     }
   });
 
+
+  //плавный переход на пункт меню
   $(".page-header__nav-list").on("click","a", function (event) {
     $(".page-header__nav-list").removeClass("open");
-    $("html, body").animate({
-      scrollTop: $($(this).attr("href")).offset().top -100 + "px"},
+    $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top -100 + "px"},
       {
         duration: 1000,
         easing: "swing"
--- src/scripts/main-nav.js
+++ src/scripts/main-nav.js
@@ -1,5 +1,6 @@
 $(document).ready( function(){
 
+  //открытые меню по клику на гамбургер
   $(".hamburger").click(function(){
     $(".page-header__nav-list").toggleClass("open");
    });
@@ -7,22 +8,23 @@ $(document).ready( function(){
   var headerTop = $('.page-header__top').offset().top;
   var headerTextPadding = $('.page-header__text').css('padding-top');
 
+  //фиксация меню
   $(window).scroll(function(){
     if( $(window).scrollTop() > headerTop ) {
       $('.container-for-fixed').addClass('fixed');
       $('.page-header__text').css('padding-top', $('.page-header__top').innerHeight());
     }
-
     else {
       $('.container-for-fixed').removeClass('fixed');
       $('.page-header__text').css('padding-top', headerTextPadding);
     }
   });
 
+
+  //плавный переход на пункт меню
   $(".page-header__nav-list").on("click","a", function (event) {
     $(".page-header__nav-list").removeClass("open");
-    $("html, body").animate({
-      scrollTop: $($(this).attr("href")).offset().top -100 + "px"},
+    $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top -100 + "px"},
       {
         duration: 1000,
         easing: "swing"
